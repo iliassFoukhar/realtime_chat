38069326f0b7c704bf599fc1a43ec18566423e0e
iliassFoukhar <nextgendevelopement@gmail.com>
2021-05-15 18:10:57 +0000

    Dynamic Focus on inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,9 @@ import './Form.css'
 export default class Form extends Component {
     state = {
         input : "",
-        password: ""
+        password: "",
+        focusPass : false,
+        focusUser : false
     }
     changeUsername = (e) => {
         this.setState({
@@ -15,8 +17,32 @@ export default class Form extends Component {
             password : e.target.value
         });
     };
+    focusUsername = (e) => {
+        this.setState({
+            focusPass : false,
+            focusUser : true
+        })
+    };
+    focusPassword = (e) => {
+        this.setState({
+            focusPass : true,
+            focusUser : false
+        })
+    };
+    
+    focusOutPassword = (e) => {
+        this.setState({
+            focusPass : false
+        });
+    };
+
+    focusOutUsername = (e) => {
+        this.setState({
+            focusUser : false
+        });
+    };
     render() {
-        const {input, password} = this.state;
+        const {input, password, focusPass, focusUser} = this.state;
         return (
             <div className="container">            
                 <div className="form">
@@ -29,24 +55,28 @@ export default class Form extends Component {
                     <h4>Or use your email account</h4>
                     <form>
                         <label>Username</label> <br/>
-                        <div className="div-group"> 
+                        <div className={focusUser == true ? "div-group active" : "div-group" }> 
                             <i className="fa fa-user fa-sm"></i>
                             <input  type="text" 
                                     value={input}
                                     className="form-control"
                                     name="username"
                                     onChange={this.changeUsername.bind(this)}
+                                    onFocus={this.focusUsername.bind(this)}
+                                    onBlur={this.focusOutUsername.bind(this)}
                                     />
                         </div>
                         <br/>
                         <label>Password</label> <br/>
-                        <div className="div-group"> 
+                        <div className={focusPass == true ? "div-group active" : "div-group" }> 
                             <i className="fa fa-lock fa-sm"></i>
                             <input  type="password" 
                                     value={password}
                                     className="form-control"
                                     name="password"
                                     onChange={this.changePassword.bind(this)}
+                                    onFocus={this.focusPassword.bind(this)}
+                                    onBlur={this.focusOutPassword.bind(this)}
                                     />
                             </div>
                         <br/>   
